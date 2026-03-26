7d339a1cb89bb8af0c5e255f819369929e73425c
Clinton Lawrence <clinton.lawrence2@gmail.com>
2026-03-26 14:32:50 -0700

    update lambda and schedule creation

--- iac/terraform.tfvars
+++ iac/terraform.tfvars
@@ -1,4 +1,4 @@
 aws_region   = "us-east-1"
 project_name = "nhl-excit-o-meter-starttime-checker"
 lambda_version = "a31ff28489b2209b3c2253325a9d97f8db5a6997"
-lambda_sched_expr = "0 8 * * ? *"
+lambda_sched_expr = "0 10 * * ? *"
--- lambda_function.py
+++ lambda_function.py
@@ -23,7 +23,7 @@ def handler(event, context):
         return "No games scheduled for today."
     
     if raw_data["gameWeek"][0]["date"] != datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d"):
-        logger.info("Games not updated for today yet! will try again in 30 minutes.")
+        logger.info("Games not updated for today yet! will try again when AWS trys again")
         time.sleep(1800)
         raise Exception("Games not updated for today yet! will try again in 30 minutes.")
     
@@ -32,10 +32,52 @@ def handler(event, context):
 
     actual_start_time_minus_5_minutes = datetime.strptime(actual_start_time, "%Y-%m-%dT%H:%M:%SZ") - timedelta(minutes=5)
 
-    scheduled_time = actual_start_time_minus_5_minutes.strftime("%Y-%m-%dT%H:%M:%S")
+    if actual_start_time_minus_5_minutes < datetime.now(datetime.timezone.utc):
+        logger.info("Time has already passed for today's game! Start Data Collection ECS now!")
+        run_ecs_task()
+    else:
 
-    logger.info(f"Time Calculated: {scheduled_time} UTC")
+        scheduled_time = actual_start_time_minus_5_minutes.strftime("%Y-%m-%dT%H:%M:%S")
+        logger.info(f"Start Time Calculated: {scheduled_time} UTC")
+        create_scheduler(scheduled_time)
 
+def run_ecs_task():
+    ecs_cluser = 'nhl-excite-o-meter-data-cluster'
+    ecs_task_definition = 'arn:aws:ecs:us-east-1:871806636838:task-definition/nhl-excite-o-meter-data-task'
+
+
+    # create ecs task scheduler to run at the start time of the first game of the day
+    ecs_client = boto3.client('ecs')
+    
+    ecs_client.run_task(
+        capacityProviderStrategy=[
+            {
+                'capacityProvider': 'FARGATE_SPOT',
+                'weight': 1,
+                'base': 0
+            },
+        ],
+        cluster=ecs_cluser,
+        count=1,
+        launchType='FARGATE',
+        networkConfiguration={
+            'awsvpcConfiguration': {
+                'subnets': [
+                    'subnet-036d25c00629af481',
+                    'subnet-0cc61dc319782a5dd'
+                ],
+                'securityGroups': [
+                'sg-0dd61ca2df34bf514',
+                ],
+                'assignPublicIp': 'DISABLED'
+            }
+        },
+        taskDefinition=ecs_task_definition,
+    )
+
+    
+
+def create_scheduler(scheduled_time):
     # create scheduler with boto3 to trigger lambda at scheduled_time
     scheduler = boto3.client('scheduler')
     
@@ -65,6 +107,10 @@ def handler(event, context):
                         'AssignPublicIp': 'ENABLED'
                     }
                 }
+                },
+                'RetryPolicy': {
+                    'MaximumEventAgeInSeconds': (3*60*60),
+                    'MaximumRetryAttempts': 185
                 },
             },
             State='ENABLED',
